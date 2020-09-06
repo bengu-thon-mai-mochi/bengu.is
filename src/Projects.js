733b9b842e8c4dbe8b4772be5814aadffe662985
mochi <benguozakinci@gmail.com>
2020-09-06 11:13:34 +0000

    Update project description

--- src/Projects.js
+++ src/Projects.js
@@ -7,7 +7,7 @@ const Projects = () => (
     <section className="projects" id="projects">
         
         <ProjectCard title="To-do list" githubRepo="https://github.com/bengu-thon-mai-mochi/To-do-List" deployLink="https://laughing-joliot-045068.netlify.app/" desc="An app to track things to do. Built with ReactJS, deployed with Netlify" thumbnail="list alternate outline"/>
-        <ProjectCard title="Github User Search" desc="An app browses through github API through user query. Built with ReactJS, deployed with Netlify." thumbnail="keyboard outline"/>
+        <ProjectCard title="Github User Search" desc="An app browses github API through user query. Built with ReactJS, deployed with Netlify." thumbnail="keyboard outline"/>
         <ProjectCard title="Share a Meal" githubRepo="https://github.com/bengu-thon-mai-mochi/share-a-meal" deployLink="https://share-a-meal.herokuapp.com/" desc="Reservation app for sharing meals. Built with Javascript, mySQL, NodeJS, ExpressJS. Deployed with Heroku." thumbnail="utensils"/>
     </section>
     </>
